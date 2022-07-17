5c23e2175da6343a4546058d3ca1ad9d21842fba
SnuteZx <snute_zx@163.com>
2022-07-17 16:06:42 +0800

    daily

--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,16 @@
 import sys
 import os
-import tempfile
-import subprocess
+import argparse
 
-class VerilogProcess(object):
+def getparer():
+    parser = argparse.ArgumentParser(description='get file')
+    parser.add_argument('-v','--version')
+    parser.add_argument('-o','--output')
+
+    return parser
+
+
+if __name__ == '__main__':
+    parser = getparer()
+    args = parser.parser_args()
+    print ("xx")
